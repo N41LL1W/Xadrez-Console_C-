ae3a033501abd6d46e3d6570039a93da92bd8d32
N41LL1W <willian.play.cel@gmail.com>
2023-10-20 13:32:00 -0300

    Testes iniciais com os movimentos das peças

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -19,7 +19,14 @@ namespace xadrez_console
 
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                    Console.Write("Destina: ");
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
                     partida.executaMovimento(origem, destino);
